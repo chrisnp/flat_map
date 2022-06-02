8f5127e0f0bf4d11a8220fff3006622dea942b4e
Invigo <invigo.devel@gmail.com>
2022-06-02 18:37:24 +0300

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,19 @@
 #[cfg(test)]
 mod tests {
+    // use super::*;
+
+    #[test]
+    fn empty() {
+        assert_eq!(flat_map(std::iter::empty(), |x: Vec<()>| {x}).count(), 0);
+    }
+    #[test]
+    fn simple() {
+        assert_eq!(flat_map(vec!["a", "b"].into_iter(), |x| {x.chars()}).count(), 2);
+    }
+    #[test]
+    fn simple_wide() {
+        assert_eq!(flat_map(vec!["a1", "bet"].into_iter(), |x| {x.chars()}).count(), 5);
+    }
     #[test]
     fn it_works() {
         let result = 2 + 2;
